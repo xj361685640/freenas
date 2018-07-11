255b5d75a8eeb4b7cf07025230c7e5ebf50c6030
Waqar Ahmed <waqarsonic1@gmail.com>
2018-07-11 06:08:57 +0500

    Renew certificate date made customizable and minor edits

--- src/middlewared/middlewared/plugins/crypto.py
+++ src/middlewared/middlewared/plugins/crypto.py
@@ -243,6 +243,11 @@ class CertificateService(CRUDService):
                 }
             )
 
+        # Remove ACME related keys if cert is not an ACME based cert
+        if not cert.get('acme'):
+            for key in ['acme', 'acme_uri', 'domains_authenticators', 'renew_days']:
+                cert.pop(key)
+
         # convert san to list
         cert['san'] = (cert.pop('san', '') or '').split()
         if cert['serial'] is not None:
@@ -722,10 +727,11 @@ class CertificateService(CRUDService):
         )
         progress = 0
         for cert in certs:
-            # TODO: Make the renew date customizable, perhaps add a field to the cert model
             progress += (100 / len(certs))
 
-            if (pytz.utc.localize(cert['expire']) - datetime.datetime.now(datetime.timezone.utc)).days < 10:
+            if (
+                    pytz.utc.localize(cert['expire']) - datetime.datetime.now(datetime.timezone.utc)
+            ).days < cert['renew_days']:
                 # renew cert
                 self.logger.debug(f'Renewing certificate {cert["name"]}')
                 final_order = self.issue_certificate(
@@ -799,6 +805,9 @@ class CertificateService(CRUDService):
     @job(lock='cert_create')
     async def do_create(self, job, data):
 
+        if not data.get('dns_mapping'):
+            data.pop('dns_mapping')  # Default dict added
+
         verrors = await self.validate_common_attributes(data, 'certificate_create')
 
         await validate_cert_name(
@@ -817,6 +826,9 @@ class CertificateService(CRUDService):
         )
 
         data['san'] = ' '.join(data.pop('san', []) or [])
+
+        # Patch creates another copy of dns_mapping
+        data.pop('dns_mapping', None)
 
         pk = await self.middleware.call(
             'datastore.insert',
@@ -1262,6 +1274,7 @@ class CertificateAuthorityService(CRUDService):
         Patch(
             'certificate_create', 'ca_create',
             ('edit', _set_enum('create_type')),
+            ('rm', {'name': 'dns_mapping'}),
             register=True
         )
     )
